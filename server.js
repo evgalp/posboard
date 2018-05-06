8c4527fb6e4fcb30135ab2e0c2a214a5aa16642a
evgalp <evgeniyalpayev@gmail.com>
2018-05-06 15:08:48 +0300

    db connection added

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Establish DB connection
+const dbUrl = require('./config/keys').dbUrl;
+
+mongoose
+    .connect(dbUrl)
+    .then(() => console.log('Mongo database connected'))
+    .catch(err => console.log(err));
 
 // Routes
 const posts = require('./routes/posts');
@@ -13,7 +28,7 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Express server is up on port ${port}`));
 
-// use routes
+// Use routes
 app.use('/routes/posts', posts);
 app.use('/routes/profile', profile);
 app.use('/routes/users', users);
